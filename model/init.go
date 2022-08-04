3178e44ea8d00637358fe6639acc2662dce4b75a
jayvee <cenjw@m.scnu.edu.cn>
2022-08-04 20:51:56 +0800

    Init

---
+++ model/init.go
@@ -0,0 +1,29 @@
+package model
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	"time"
+)
+
+// DB 全局数据库操作变量
+var DB *gorm.DB
+
+func Database(connstring string) {
+	println(connstring)
+	db, err := gorm.Open("mysql", connstring)
+	if err != nil {
+		panic("MySQL数据库连接错误")
+	}
+	fmt.Println("数据库连接成功！")
+	db.LogMode(true)
+	if gin.Mode() == "release" {
+		db.LogMode(false)
+	}
+	db.SingularTable(true)       // 表名不加s，user表，非users表
+	db.DB().SetMaxIdleConns(20)  //设置连接池
+	db.DB().SetMaxOpenConns(100) //最大连接数
+	db.DB().SetConnMaxIdleTime(time.Second * 30)
+	DB = db
+}
