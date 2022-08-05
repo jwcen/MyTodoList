28747f7d2e62e4ec524fb6b2b86c3c74750ff0d8
jayvee <cenjw@m.scnu.edu.cn>
2022-08-05 17:50:19 +0800

    Create routers.go

---
+++ routers/routers.go
@@ -0,0 +1,25 @@
+package routers
+
+import (
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+
+	"MyTodoList/api"
+)
+
+func NewRouter() *gin.Engine {
+	r := gin.Default()
+	store := cookie.NewStore([]byte("something-very-secret"))
+	r.Use(sessions.Sessions("mysession", store))  // 存储session
+
+	// 编写基础路由
+	v1 := r.Group("api/v1")
+	{
+		// 用户操作
+		v1.POST("user/register", api.UserRegister)
+		v1.POST("user/login", api.UserLogin)
+	}
+
+	return r
+}
