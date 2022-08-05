d68a31fa082252af38e70832304ffcac87c9b837
jayvee <cenjw@m.scnu.edu.cn>
2022-08-05 17:50:50 +0800

    Update user.go

--- model/user.go
+++ model/user.go
@@ -1,10 +1,30 @@
 package model
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
+)
 
 // User 用户表
 type User struct {
 	gorm.Model
 	UserName       string `gorm:"unique"`
 	PasswordDigest string // 存储的是密文
+}
+
+
+// SetPassword 密码加密
+func (u *User) SetPassword(password string) error {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 5)
+	if err != nil {
+		return err
+	}
+	u.PasswordDigest = string(bytes)
+	return nil
+}
+
+// CheckPassword 验证密码
+func (u *User) CheckPassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.PasswordDigest), []byte(password))
+	return err == nil
 }
